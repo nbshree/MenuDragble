4e11e6b98bf50178dde706e548afa50d4f0ba8e2
nbshree <401163814@qq.com>
2021-06-03 14:50:18 +0800

    分组优化

--- src/pages/SiderDemo.tsx/DragbleMenu/index.tsx
+++ src/pages/SiderDemo.tsx/DragbleMenu/index.tsx
@@ -1,6 +1,13 @@
-import React, { createContext, useRef, useState, useContext } from "react";
+import React, {
+  createContext,
+  useRef,
+  useState,
+  useContext,
+  Dispatch,
+  SetStateAction
+} from "react";
 import { Menu, SubMenuProps } from "antd";
-import { useDrag, useDrop } from "react-dnd";
+import { DropTargetMonitor, useDrag, useDrop, XYCoord } from "react-dnd";
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -16,6 +23,7 @@ import styles from "./index.less";
 
 interface CtxProps {
   sort?: (dragId: string, hoverId: string) => void;
+  setOpenKeys?: Dispatch<SetStateAction<string[]>>;
 }
 
 const Ctx = createContext<CtxProps>({});
@@ -26,13 +34,18 @@ interface ItemType {
   key: string;
   icon?: React.ReactNode;
   title: string;
+  type: "form" | "group";
   children?: ItemType[];
 }
 
 const useSort = ({ id }: { id: string }) => {
-  const { sort } = useContext(Ctx);
+  const ref = useRef<HTMLDivElement>(null);
+  const { sort, setOpenKeys } = useContext(Ctx);
   const [{ isDragging }, drag] = useDrag({
     item: { type: "menuItem", key: id },
+    // begin: () => {
+    //   setOpenKeys && setOpenKeys([]);
+    // },
     collect: (monitor: any) => ({
       isDragging: monitor.isDragging()
     })
@@ -54,14 +67,13 @@ const useSort = ({ id }: { id: string }) => {
     drag,
     drop,
     isOver,
-    isDragging
+    isDragging,
+    ref
   };
 };
 
 function DragMenuItem(props: MenuItemProps) {
-  const ref = useRef<any>(null);
-
-  const { drag, drop, isOver, isDragging } = useSort({
+  const { drag, drop, isOver, isDragging, ref } = useSort({
     id: props.eventKey as string
   });
   drag(drop(ref));
@@ -80,27 +92,8 @@ function DragMenuItem(props: MenuItemProps) {
 }
 
 function DragSubMenu(props: MenuItemProps) {
-  const ref = useRef<any>(null);
-
-  // const { drag, drop, isOver, isDragging } = useSort({
-  //   id: props.eventKey as string,
-  // });
-  const [{ isDragging }, drag] = useDrag({
-    item: { type: "menuItem", key: props.eventKey as string },
-    collect: (monitor: any) => ({
-      isDragging: monitor.isDragging()
-    })
-  });
-  const [{ isOver }, drop] = useDrop({
-    accept: "menuItem",
-    drop: (item: any) => {
-      // if (sort && id && isOver) {
-      //   sort(item.key, id);
-      // }
-    },
-    collect: monitor => ({
-      isOver: monitor.isOver({ shallow: true })
-    })
+  const { drag, drop, isOver, isDragging, ref } = useSort({
+    id: props.eventKey as string
   });
   drag(drop(ref));
   const divStyle: any = {};
@@ -112,38 +105,46 @@ function DragSubMenu(props: MenuItemProps) {
   }
   return (
     <div ref={ref} style={divStyle}>
-      <SubMenu {...props}></SubMenu>
+      <SubMenu {...props} />
     </div>
   );
 }
 
 const DragbleMenu: React.FC = () => {
+  const [openKeys, setOpenKeys] = useState<string[]>([]);
   const [list, setList] = useState<ItemType[]>([
     {
       key: "1",
       icon: <PieChartOutlined />,
+      type: "form",
       title: "Option 1"
     },
     {
       key: "2",
       icon: <DesktopOutlined />,
+      type: "form",
       title: "Option 2"
     },
     {
       key: "sub1",
       icon: <UserOutlined />,
-      title: "User",
+      type: "group",
+      title: "group1",
       children: [
         {
           key: "3",
+          type: "form",
           title: "Tom"
         },
         {
           key: "4",
+          icon: <DesktopOutlined />,
+          type: "form",
           title: "Bill"
         },
         {
           key: "5",
+          type: "form",
           title: "Alex"
         }
       ]
@@ -151,14 +152,17 @@ const DragbleMenu: React.FC = () => {
     {
       key: "sub2",
       icon: <TeamOutlined />,
-      title: "Team",
+      type: "group",
+      title: "group2",
       children: [
         {
           key: "6",
+          type: "form",
           title: "Team 1"
         },
         {
           key: "8",
+          type: "form",
           title: "Team 2"
         }
       ]
@@ -166,17 +170,19 @@ const DragbleMenu: React.FC = () => {
     {
       key: "9",
       icon: <FileOutlined />,
-      title: "Files"
+      type: "group",
+      title: "group3"
     }
   ]);
 
   const getPosition = (id: string) => {
-    let p: { index: number; innerIndex?: number } | undefined;
+    let p: { index: number; innerIndex?: number; data: ItemType } | undefined;
 
     list.some((item, index) => {
       if (item.key === id) {
         p = {
-          index: index
+          index: index,
+          data: item
         };
         return true;
       }
@@ -186,7 +192,8 @@ const DragbleMenu: React.FC = () => {
         if (innerIndex > -1) {
           p = {
             index: index,
-            innerIndex: innerIndex
+            innerIndex: innerIndex,
+            data: item.children.find(child => child.key === id)!
           };
         }
       }
@@ -203,9 +210,46 @@ const DragbleMenu: React.FC = () => {
     const dragPosition = getPosition(dragId);
     console.log(dragId);
     console.log(dragPosition);
+    // if (dragPosition?.data?.type === "group") {
+    //   setOpenKeys([]);
+    // }
     const dropPosition = getPosition(hoverId);
     console.log(hoverId);
     console.log(dropPosition);
+
+    if (
+      dragPosition?.data.type !== "group" &&
+      dropPosition?.data.type === "group" &&
+      isNumber(dragPosition?.index) &&
+      !isNumber(dragPosition?.innerIndex)
+    ) {
+      if (
+        dropPosition?.data.children &&
+        dropPosition?.data.children.length > 0
+      ) {
+        return;
+      }
+      const updatedList = update(list, {
+        $splice: [[dragPosition?.index || 0, 1]],
+        [dropPosition.index]: {
+          children: {
+            $apply: (v: any) => {
+              return [dragPosition?.data, ...(v || [])];
+            }
+          }
+        }
+      });
+      setList(updatedList);
+      return;
+    }
+
+    if (
+      dragPosition?.data.type === "group" &&
+      dropPosition?.data.type !== "group" &&
+      isNumber(dropPosition?.innerIndex)
+    ) {
+      return;
+    }
 
     // 一级间的sort
     if (
@@ -240,14 +284,14 @@ const DragbleMenu: React.FC = () => {
     ) {
       console.log("同一个文件夹下拖拽");
       const dragItem = list[dragPosition.index].children![
-        dragPosition.innerIndex
+        dragPosition.innerIndex || 0
       ];
       const updatedList = update(list, {
         [dragPosition.index]: {
           children: {
             $splice: [
-              [dragPosition.innerIndex, 1],
-              [dropPosition.innerIndex, 0, dragItem]
+              [dragPosition.innerIndex || 0, 1],
+              [dropPosition.innerIndex || 0, 0, dragItem]
             ]
           }
         }
@@ -268,17 +312,17 @@ const DragbleMenu: React.FC = () => {
     ) {
       console.log("不同文件夹下拖拽");
       const dragItem = list[dragPosition.index].children![
-        dragPosition.innerIndex
+        dragPosition.innerIndex || 0
       ];
       const updatedList = update(list, {
         [dragPosition.index]: {
           children: {
-            $splice: [[dragPosition.innerIndex, 1]]
+            $splice: [[dragPosition.innerIndex || 0, 1]]
           }
         },
         [dropPosition.index]: {
           children: {
-            $splice: [[dropPosition.innerIndex, 0, dragItem]]
+            $splice: [[dropPosition.innerIndex || 0, 0, dragItem]]
           }
         }
       });
@@ -297,7 +341,7 @@ const DragbleMenu: React.FC = () => {
     ) {
       console.log("文件夹内拖至外部一级");
       const dragItem = list[dragPosition.index].children![
-        dragPosition.innerIndex
+        dragPosition.innerIndex || 0
       ];
       const updatedList = update(list, {
         [dragPosition.index]: {
@@ -337,12 +381,16 @@ const DragbleMenu: React.FC = () => {
 
   return (
     <DndProvider>
-      <Ctx.Provider value={{ sort: sort }}>
+      <Ctx.Provider value={{ sort, setOpenKeys }}>
         <Menu
           theme="dark"
-          defaultSelectedKeys={["1"]}
+          // openKeys={openKeys}
+          // defaultSelectedKeys={["1"]}
           mode="inline"
           className={styles.main}
+          // onOpenChange={(keys: React.Key[]) => {
+          //   setOpenKeys(keys.map(item => item.toString()));
+          // }}
         >
           {list.map(item => {
             if (item.children?.length) {
